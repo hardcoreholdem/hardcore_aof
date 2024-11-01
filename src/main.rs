1ff421bd420f770882b01b9445ee71e95936442c
hardcoreholdem <hardcoreholdem@gmail.com>
2024-11-01 16:06:58 +0800

    add cmd line to select section

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
 use hardcore_equitizer::Equitizer;
 use hardcore_equitizer::HandRanker;
 
-    pub mod all_combos;
-    pub mod section1;
-    pub mod section2;
-    pub mod section3;
-    pub mod section4;
-    pub mod section5;
-    pub mod section6;
+pub mod all_combos;
+pub mod section1;
+pub mod section2;
+pub mod section3;
+pub mod section4;
+pub mod section5;
+pub mod section6;
 
 use all_combos::calc_all_combos;
 use section1::section1;
@@ -16,19 +16,30 @@ use section3::section3;
 use section4::section4;
 use section5::section5;
 use section6::section6;
+
+use clap::Parser;
+
+#[derive(Parser)]
+struct Cli {
+    #[arg(short, long)]
+    section: u32,
+}
+
 fn main() {
     println!("Hello, world!");
 
     let hand_hanker = HandRanker::new("data").unwrap();
     let mut equitizer = Equitizer::new(&hand_hanker);
 
-    println!("开始");
+    let cli = Cli::parse();
 
-    // section1(&mut equitizer);
-    // section2(&mut equitizer);
-    // section3(&mut equitizer);
-    // section4(&mut equitizer);
-    section5(&mut equitizer);
-    section6(&mut equitizer);
-    println!("结束");
+    match cli.section {
+        1 => section1(&mut equitizer),
+        2 => section2(&mut equitizer),
+        3 => section3(&mut equitizer),
+        4 => section4(&mut equitizer),
+        5 => section5(&mut equitizer),
+        6 => section6(&mut equitizer),
+        _ => panic!("invalid section"),
+    }
 }
