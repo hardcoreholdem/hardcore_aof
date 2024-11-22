b2db5524f9870cef6c56a9080989150446842abf
hardcoreholdem <hardcoreholdem@gmail.com>
2024-11-22 20:17:31 +0800

    refactor

--- src/aux.rs
+++ src/aux.rs
@@ -8,8 +8,8 @@ mod calc_beta_1d;
 mod calc_beta_2d;
 mod calc_beta_3d;
 mod calc_eq_1d;
+mod calc_eq_2d;
 mod calc_eq_3d;
-mod calc_eq_4d;
 mod calc_s;
 mod join_calc_s_and_beta;
 mod solve_linear_eq_1d;
@@ -25,7 +25,7 @@ pub use calc_beta_1d::calc_beta_1d;
 pub use calc_beta_2d::calc_beta_2d;
 pub use calc_beta_3d::calc_beta_3d;
 pub use calc_eq_1d::calc_eq_1d;
-pub use calc_eq_3d::calc_eq_2d; // TODO: rename
-pub use calc_eq_4d::calc_eq_3d; // TODO:rename
+pub use calc_eq_2d::calc_eq_2d;
+pub use calc_eq_3d::calc_eq_3d;
 pub use calc_s::calc_s;
 pub use join_calc_s_and_beta::join_calc_s_and_beta;
--- src/aux/calc_eq_3d.rs
+++ src/aux/calc_eq_3d.rs
@@ -1,13 +1,15 @@
 // TODO: rename file
-pub fn calc_eq_2d(
+pub fn calc_eq_3d(
     (p0, eq0): (f64, f64),
     (p1, eq1): (f64, f64),
     (p2, eq2): (f64, f64),
+    (p3, eq3): (f64, f64),
     scale_1: f64,
     scale_2: f64,
+    scale_3: f64,
 ) -> f64 {
-    let num = p0 * eq0 + scale_1 * p1 * eq1 + scale_2 * p2 * eq2;
-    let den = p0 + scale_1 * p1 + scale_2 * p2;
+    let num = p0 * eq0 + scale_1 * p1 * eq1 + scale_2 * p2 * eq2 + scale_3 * p3 * eq3;
+    let den = p0 + scale_1 * p1 + scale_2 * p2 + scale_3 * p3;
 
     num / den
 }
